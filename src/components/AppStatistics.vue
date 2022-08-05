3b63f2c7837f0824be24a05a0a928809b9cfbedb
Максим Волков <maksik-maksik-2001@mail.ru>
2022-08-05 23:21:17 +0300

    task1, task2: выполнил 1 и 2 задания

---
+++ src/components/AppStatistics.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="statistics-wrapper">
+    <div class="statistics-wrapper__item">
+      Completed tasks {{ completedTasks }}
+    </div>
+    <div class="statistics-wrapper__item">All tasks {{ allTasks }}</div>
+    <div class="statistics-wrapper__item">
+      Percent complete tasklist {{ percent }} %
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppStatistics",
+  props: {
+    items: Array,
+  },
+  computed: {
+    allTasks() {
+      return this.items.length;
+    },
+    completedTasks() {
+      let completed = 0;
+      for (let i = 0; i < this.items.length; i++) {
+        if (this.items[i].done == true) {
+          completed++;
+        }
+      }
+      return completed;
+    },
+    percent() {
+      return Math.ceil((this.completedTasks / this.allTasks) * 100);
+    },
+  },
+};
+</script>
